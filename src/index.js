32bf072548aba1fb7f599c088bdc72ab5794ed1c
vagnerzelom <vagnerzelom@hotmail.com>
2020-08-09 17:28:36 -0300

    criar rota post e testado

--- src/index.js
+++ src/index.js
@@ -5,10 +5,26 @@ const app = express();
 
 app.use(express.json());
 
-const list=[];
+let lists={
+    devoloper:[]
+};
 
 app.get("/devoloper",(req,res)=>{
+    return res.json(lists);
+})
+
+app.post("/devoloper",(req,res)=>{
+    const id = uuid();
+    const {name,lastName,years,company,technologies}=req.body;
+
+    const list = {id,name,lastName,years,company,technologies};
+
+    lists.devoloper.push(list)
+
+    
+
     return res.json(list);
+
 })
 
 
