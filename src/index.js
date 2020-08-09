ddaffa735bd2b4c4abcc5c35adbebdecfdf4a734
vagnerzelom <vagnerzelom@hotmail.com>
2020-08-09 17:53:13 -0300

    criar rota put e testado

--- src/index.js
+++ src/index.js
@@ -27,6 +27,24 @@ app.post("/devoloper",(req,res)=>{
 
 })
 
+app.put("/devoloper/:id",(req,res)=>{
+    const {id} =req.params;
+
+    const {name,lastName,years,company,technologies}= req.body;
+
+    const listIndex = lists.devoloper.findIndex((list)=>list.id == id);
+
+    if(listIndex < 0){
+        return res.status(400).json({ERROR:"this programmer does not exist"});
+        }
+     const list = {id,name,lastName,years,company,technologies};
+     
+     lists.devoloper[listIndex]=list;
+
+     return res.json(list);
+    
+})
+
 
 
 const port = 3344;
